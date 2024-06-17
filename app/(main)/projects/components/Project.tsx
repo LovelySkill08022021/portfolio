b1c9b29c9c674b30615d5f0f031adbc6c7e358ab
LovelySkill08022021 <markjustinecudapas@gmail.com>
2024-06-17 13:28:02 +0800

    new projects added

--- app/(main)/projects/components/Project.tsx
+++ app/(main)/projects/components/Project.tsx
@@ -1,6 +1,9 @@
+'use client';
+
 import Image from 'next/image';
 import './Project.css'
 import Link from 'next/link';
+import { useEffect, useId, useState } from 'react';
 
 type Props = {
     title: string;
@@ -10,6 +13,38 @@ type Props = {
     link?: string;
 }
 export default function Project({ title, description, tools_used, image_url, link="#" } : Props){
+
+    const [description_clamp, setDescriptionClamp] = useState<any>("unset");
+    const [see_more_button_visibility, setSeeMoreButtonVisibility] = useState<boolean>(false);
+    const clamper_id = useId();
+
+    function toggleDescriptionClamp(){
+        if(description_clamp == 2){
+            setDescriptionClamp('unset');
+        } else {
+            setDescriptionClamp(2);
+        }
+    }
+
+    function SeeMoreButton(){
+        return (
+            <div className='text-slate-600 cursor-pointer mt-1' onClick={toggleDescriptionClamp}>
+                {description_clamp == 2 ? "See more" : "See less"}
+            </div>
+        )
+    }
+
+    useEffect(() => {
+        let clamper = document.getElementById(clamper_id);
+        if(clamper){
+            if(clamper.clientHeight > 40){
+                setSeeMoreButtonVisibility(true);
+                setDescriptionClamp(2)
+            }
+        }
+
+    },[]);
+
     return (
         <>
             <div className='project lg:w-11/12'>
@@ -17,17 +52,20 @@ export default function Project({ title, description, tools_used, image_url, lin
                     <div className='project-title'>
                         {title}
                     </div>
-                    <div className='project-description'>
+                    <div id={clamper_id} className={'project-description'} style={{
+                        WebkitLineClamp: description_clamp
+                    }}>
                         {description}
                     </div>
+                    {see_more_button_visibility && <SeeMoreButton />}
                     {(link != "#" ) && (
                         <div className='mt-3'>
                             <Link className='text-blue-500' href={link} target='_blank' >Visit</Link>
                         </div>
                     )}
                     {tools_used && (
-                        <div className='mt-5'>
-                            {tools_used}
+                        <div className='mt-5 text-slate-500'>
+                            <span>Built with:</span> <br /> {tools_used}
                         </div>
                     )}  
                 </div>
--- app/(main)/projects/page.tsx
+++ app/(main)/projects/page.tsx
@@ -3,6 +3,7 @@ import ToolIcon from "./components/ToolIcon";
 import './ProjectPage.css'
 
 export default function ProjectPage(){
+
     return (
         <>
             <div className="block lg:flex w-full justify-between">
@@ -10,7 +11,7 @@ export default function ProjectPage(){
                     <div className="project-header level-1-h">
                         My Projects
                     </div>
-                    <Project title="DTrail" description="Designed to enhance productivity, improve information governance, and facilitate efficient knowledge management within organizations. By providing a robust framework for tagging, storing, and locating documents, DTRIL ensures that valuable information is always at your fingertips." tools_used={
+                    <Project title="DTrail" description="Designed to enhance productivity, improve information governance, and facilitate efficient knowledge management within organizations. By providing a robust framework for tagging, storing, and locating documents, DTrail ensures that valuable information is always at your fingertips." tools_used={
                         <>
                             <ToolIcon icon_url="/logos/nodejs.png" title="nodejs" />
                             <ToolIcon icon_url="/logos/laravel.png" title="laravel" />
@@ -18,17 +19,41 @@ export default function ProjectPage(){
                             <ToolIcon icon_url="/logos/inertia.png" title="inertia" />
                         </>
                     } image_url="/project_images/dtrail.jpg" link="https://dtrail.mmsu.edu.ph/" />
+
+                    <Project title="Farm Machinery Management System" description="A comprehensive, digital platform designed to optimize the management, maintenance, and operation of farm machinery. This system integrates various functionalities to streamline agricultural processes, improve efficiency, and reduce operational costs." tools_used={
+                        <>
+                            <ToolIcon icon_url="/logos/laravel.png" title="laravel" />
+                        </>
+                    } image_url="/project_images/machinery2.jpg" />
+                    
+                    <Project title="Integrated Farm Analytics" description="An advanced digital platform designed to provide comprehensive, data-driven insights into all aspects of farm management. This system consolidates data from various farming operations, including crop production, livestock management, machinery usage, and environmental factors, to enhance decision-making, improve efficiency, and promote sustainability." tools_used={
+                        <>
+                            <ToolIcon icon_url="/logos/laravel.png" title="laravel" />
+                        </>
+                    } image_url="/project_images/ida1.jpg" />
                     
                 </div>
                 <div className="w-full">
                     <Project title="Scorr" description="Scorr revolutionizes student score recording by integrating QR code technology with ready-to-print export formats, providing a fast and accurate system for educational institutions. Whether for daily assessments, exams, or comprehensive evaluations, Scorr offers a cutting-edge solution that enhances efficiency, accuracy, and insight, ensuring that score data is always easily accessible and presentable." tools_used={
                         <>
                             <ToolIcon icon_url="/logos/nodejs.png" title="nodejs" />
-                            <ToolIcon icon_url="/logos/nextjs.png" title="nextjs" />
+                            <ToolIcon icon_url="/logos/next.png" title="nextjs" />
                             <ToolIcon icon_url="/logos/clerk.png" title="clerk" />
                             <ToolIcon icon_url="/logos/ts.png" title="typescript" />
                         </>
                     } image_url="/project_images/scorr.jpg" />
+                    
+                    <Project title="Piggery Management System" description="A specialized digital platform designed to optimize the management of pig farming operations. This system facilitates the efficient handling of various aspects of pig farming, including animal health, breeding, feeding, and environmental management, ensuring better productivity, profitability, and sustainability." tools_used={
+                        <>
+                            <ToolIcon icon_url="/logos/laravel.png" title="laravel" />
+                        </>
+                    } image_url="/project_images/piggery3.jpg" />
+                    
+                    <Project title="Poultry Management System" description="A sophisticated digital platform designed to streamline and optimize the management of poultry farming operations. This system addresses various aspects of poultry farming, including flock management, health monitoring, feeding, egg production, and environmental control, to enhance productivity, profitability, and sustainability." tools_used={
+                        <>
+                            <ToolIcon icon_url="/logos/laravel.png" title="laravel" />
+                        </>
+                    } image_url="/project_images/poultry3.jpg" />
                     
                     
                 </div>
